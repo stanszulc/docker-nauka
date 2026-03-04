da82d25e954348fa0116096974d54f8955296ef5
stanszulc <30842830+stanszulc@users.noreply.github.com>
2026-03-04 15:13:53 +0000

    Aktualizacja plików streamów: timestamp, GPS

--- streams/click/click_stream.py
+++ streams/click/click_stream.py
@@ -1,7 +1,15 @@
+# click_stream.py
 import time
 from datetime import datetime
+import random
 
 while True:
-    print(f"[{datetime.now()}] CLICK event")
-    time.sleep(1)
-    
+    event = {
+        "user_id": random.randint(1, 1000),
+        "action": "click",
+        "timestamp": str(datetime.now()),
+        "gps": {"lat": round(random.uniform(50.0, 50.1), 6),
+                "lon": round(random.uniform(19.9, 20.0), 6)}
+    }
+    print(event)
+    time.sleep(3)  # co 3 sekundy nowy event
--- streams/purchase/purchase_stream.py
+++ streams/purchase/purchase_stream.py
@@ -1,8 +1,16 @@
+# purchase_stream.py
 import time
 from datetime import datetime
 import random
 
 while True:
-    amount = random.randint(20, 500)
-    print(f"[{datetime.now()}] PURCHASE {amount} PLN")
-    time.sleep(4)
+    event = {
+        "user_id": random.randint(1, 1000),
+        "action": "purchase",
+        "amount": round(random.uniform(10, 500), 2),
+        "timestamp": str(datetime.now()),
+        "gps": {"lat": round(random.uniform(50.0, 50.1), 6),
+                "lon": round(random.uniform(19.9, 20.0), 6)}
+    }
+    print(event)
+    time.sleep(5)  # co 5 sekund nowy event
