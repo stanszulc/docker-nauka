0b29271e97599f1dce197333359a81331e027fb3
stanszulc <30842830+stanszulc@users.noreply.github.com>
2026-03-04 15:13:53 +0000

    Aktualizacja plików streamów: timestamp, GPS

--- streams/login/login_stream.py
+++ streams/login/login_stream.py
@@ -1,6 +1,15 @@
+# login_stream.py
 import time
 from datetime import datetime
+import random
 
 while True:
-    print(f"[{datetime.now()}] LOGIN event")
-    time.sleep(2)
+    event = {
+        "user_id": random.randint(1, 1000),
+        "action": "login",
+        "timestamp": str(datetime.now()),
+        "gps": {"lat": round(random.uniform(50.0, 50.1), 6),
+                "lon": round(random.uniform(19.9, 20.0), 6)}
+    }
+    print(event)
+    time.sleep(2)  # co 2 sekundy nowy event
